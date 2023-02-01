d0640ff6ca3e3207a7879e17da880a892aef096d
NChirushina@gmail.com <NChirushina@gmail.com>
2023-02-02 00:49:13 +0300

    request added

---
+++ MindboxDb/MindboxDb/QueryResults/ProductsWithCategories.cs
@@ -0,0 +1,13 @@
+﻿namespace MindboxDb.QueryResults
+{
+    public class ProductsWithCategories
+    {
+        public Guid? ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public Guid? CategotyId { get; set; }
+
+        public string CategoryName { get; set; }
+    }
+}
---
+++ MindboxDb/MindboxDb/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using MindboxDb.QueryResults;
+
+namespace MindboxDb.Repositories
+{
+    public interface IProductRepository
+    {
+        /// <summary>
+        /// Запрос для выбора всех пар «Имя продукта – Имя категории». Если у продукта нет категорий, то его имя все равно выводится.
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<ProductsWithCategories>> GetAllProductsAsync();
+    }
+}
